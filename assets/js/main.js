5dd7ddb07968b34f65df32d0f11b1db025cbb5bd
danknjoroge <dank8505@gmail.com>
2022-06-24 20:46:05 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 // *** Write your code here***
+function calcNumbers(val){
+    document.getElementById('result').value += val
+    return val
+}
+
+function clearScreen(){
+    document.getElementById('result').value = ''
+}
+
+function deleted(){
+    var val = document.getElementById('result').value.slice(0, -1);
+    document.getElementById('result').value = val;  
+}
+
+
+
+
+
